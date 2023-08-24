5591f22443d9bffae8bbe01a1b983b55318f474d
João Vitor Rodrigues Santos <joaovitorrodriguessantos8@outlook.com>
2023-08-24 17:40:33 -0300

    função add linha tabela

--- Controller/openModal.js
+++ Controller/openModal.js
@@ -4,15 +4,21 @@ var modalDesp = document.getElementById('modalDesp');
 var btnClose_Desp = document.getElementById('btnClose_Desp');
 var btnClole_Rece = document.getElementById('btnClose_Rece');
 
-const salvarR = document.getElementById("salvarR");
+
 let receitasIP = document.getElementById("receitas-Global");
 let despesasIP = document.getElementById("despesas-Global");
 const saldoGeralP = document.getElementById("saldo-Global");
 
+const salvarR = document.getElementById("salvarR");
 const valorReceInput = document.getElementById("valorRece");
+const dataRece = document.getElementById("dataRece");
+const descRece = document.getElementById("descRece");
+
 
 const salvarD = document.getElementById("salvarD");
 const valorDespInput = document.getElementById("valorDesp");
+const dataDesp = document.getElementById("dataDesp");
+const descDesp = document.getElementById("descDesp");
 
 
 btnDesp.addEventListener("click", function() {
@@ -33,8 +39,37 @@ let saldoGeral = 0;
 let receita = 0;
 let despesa = 0;
 
+var tableD = document.getElementById('tabela_despesas');
+
+ 
+
+function adicionaLinha(idTable, valor, descri, datas) {
+
+  var tabela = document.getElementById('tabela_despesas');
+  var numLinhas = tabela.rows.length;
+  var linha = tabela.insertRow(numLinhas);
+  var buttons = linha.insertCell(0);
+  var despesa = linha.insertCell(1);
+  var desc = linha.insertCell(2);
+  var data = linha.insertCell(3);
+
+  buttons.innerHTML = "<button onclick='removeLinha(this)'>Remover</button>";
+  despesa.innerHTML = "R$" + valor.value;
+  desc.innerHTML = descri.value;
+  data.innerHTML = datas.value;
+
+
+
+}
+
+function removeLinha(linha) {
+  var i=linha.parentNode.parentNode.rowIndex;
+  document.getElementById('tabela_despesas').deleteRow(i);
+}         
+
+
 salvarR.addEventListener("click", function() {
-        
+
     console.log("Funciona");
     const novaReceita = parseFloat(valorReceInput.value);
       console.log(novaReceita)
@@ -44,6 +79,9 @@ salvarR.addEventListener("click", function() {
         receitasIP.textContent = `R$+ ${receita.toFixed(2)}`;
         saldoGeralP.textContent = `R$ ${saldoGeral.toFixed(2)}`;
         valorReceInput.value = "";
+
+        
+
         modalRece.close();
       }
 
@@ -65,6 +103,7 @@ salvarD.addEventListener("click", function() {
         modalDesp.close();
       }
 
+      adicionaLinha(tableD,valorDespInput,descDesp,dataDesp);
     
 });
 
